c2592eabe5cfaea0409b74d53ff78f3fbb05187b
Garrett <garrettchinian@gmail.com>
2023-01-22 03:42:20 -0500

    Set up Animations
    -Started to work on door animations with interactions
    -Door closing isn't quite working
    -Still buggy

--- Project A/Assets/Folders/Assets/Scripts/Enviro/WheelInteract.cs
+++ Project A/Assets/Folders/Assets/Scripts/Enviro/WheelInteract.cs
@@ -8,6 +8,18 @@ public class WheelInteract : Interactable
     public Material highColor;
     public Material regColor;
 
+    private bool doorOpen = false;
+
+    [SerializeField] private Animator doorAnim = null;
+    [SerializeField] private string openAnimationName = "Wheel_Open";
+    [SerializeField] private string closeAnimationName = "Wheel_Close";
+
+    [SerializeField] private int waitTimer = 1;
+    [SerializeField] private bool pauseInteraction = false;
+
+    public GameObject door;
+    DoorAnima a;
+
     public override void OnFocus()
     {
         GetComponent<MeshRenderer>().material = highColor;
@@ -15,11 +27,37 @@ public class WheelInteract : Interactable
 
     public override void OnInteract()
     {
-        print("Interacted with gameObject.name");
+        if (!doorOpen && !pauseInteraction)
+        {
+            doorAnim.Play(openAnimationName, 0, 0.0f);
+            doorOpen = true;
+            StartCoroutine(PauseDoorInteraction());
+            //a.MethodA(gameObject);
+        }
+
+        else if (doorOpen && !pauseInteraction)
+        {
+            doorAnim.Play(closeAnimationName, 0, 0.0f);
+            doorOpen = false;
+            StartCoroutine(PauseDoorInteraction());
+            //a.MethodB(gameObject);
+        }
     }
 
     public override void OnLoseFocus()
     {
         GetComponent<MeshRenderer>().material = regColor;
+    }
+
+    private IEnumerator PauseDoorInteraction()
+    {
+        pauseInteraction = true;
+        yield return new WaitForSeconds(waitTimer);
+        pauseInteraction = false;
+    }
+
+    public void Start()
+    {
+        a = GameObject.FindGameObjectWithTag("TagDoor").GetComponent<DoorAnima>();
     }
 }
